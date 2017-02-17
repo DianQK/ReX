d8500ac5859cba70ee1c382ff6e3d1c518cca14b
蔡学敏 <382499488@qq.com>
2017-02-17 20:49:26 +0800

    fix Template type

--- Template/ReX/Store.xctemplate/___FILEBASENAME___Store.swift
+++ Template/ReX/Store.xctemplate/___FILEBASENAME___Store.swift
@@ -8,18 +8,18 @@
 
 import ReX
 
-class ___FILEBASENAMEASIDENTIFIER___Store: ReX.Store {
+class ___FILEBASENAMEASIDENTIFIER___Store: ReX.StoreType {
 
 }
 
-extension ReX.Getter where Base: ___FILEBASENAMEASIDENTIFIER___Store {
+extension ReX.Getter where Store: ___FILEBASENAMEASIDENTIFIER___Store {
 
 }
 
-extension ReX.Mutation where Base: ___FILEBASENAMEASIDENTIFIER___Store {
+extension ReX.Mutation where Store: ___FILEBASENAMEASIDENTIFIER___Store {
 
 }
 
-extension ReX.Action where Base: ___FILEBASENAMEASIDENTIFIER___Store {
+extension ReX.Action where Store: ___FILEBASENAMEASIDENTIFIER___Store {
 
 }
